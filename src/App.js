900094ab2d99da06af86bba974101d503c1f47b3
Lucas Vieira Gonçalves <99230215+lucasvieirapcr@users.noreply.github.com>
2024-05-15 22:04:05 -0300

    creating a simple event website

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Banner from './components/Banner';
+import TextField from './components/TextField';
+
+function App() {
+  return (
+    <>
+      <Banner />
+      <TextField  />
+      <TextField  />
+      <TextField  />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Banner/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './Banner.css'
+
+function Banner(){
+    return(
+        <div className="banner-container">
+            <img className="banner" src="/images/banner.PNG" alt="O banner principal da pagina Nexus Events"></img>
+        </div>
+    )
+}
+
+export default Banner
